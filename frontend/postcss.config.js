b705d1fd2197a9f051168a9ea41f37a3edd0f0b5
pyramidsmartsales-hue <pyramids.mart.sales@gmail.com>
2025-11-08 02:02:41 +0300

    5

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,7 +1,7 @@
 // pyramids-mart/frontend/postcss.config.js
+import tailwindcss from "@tailwindcss/postcss";
+import autoprefixer from "autoprefixer";
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [tailwindcss(), autoprefixer()],
+};
